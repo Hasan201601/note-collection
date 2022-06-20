e79deaa4a31b818d6883ec88124054bfcb26e818
Hasan <hasanuzzaman201601@gmail.com>
2022-06-21 00:07:24 +0600

    ProcFile updated

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
-const port = process.env.PORT || 5000;
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 const noteRoutes = require("./routes/noteRoutes");
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 const cors = require("cors");
 const path = require("path");
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 
